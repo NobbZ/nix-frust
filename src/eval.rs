9d3313f2a9e37bb1b417b1ea2e27ce04c3c72308
Norbert Melzer <timmelzer@gmail.com>
2023-08-30 07:58:33 +0200

    feat: floats

--- src/eval.rs
+++ src/eval.rs
@@ -4,9 +4,10 @@ use nil_syntax::{
     parser,
 };
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq)]
 pub enum Value {
     Integer(i64),
+    Float(f64),
 }
 
 pub fn code(code: &str) -> Result<Value> {
@@ -30,6 +31,7 @@ fn eval_expr(expr: &Expr) -> Result<Value> {
 fn eval_literal(lit: &Literal) -> Result<Value> {
     match lit.kind().unwrap() {
         LiteralKind::Int => Ok(Value::Integer(lit.token().unwrap().text().parse::<i64>()?)),
+        LiteralKind::Float => Ok(Value::Float(lit.token().unwrap().text().parse::<f64>()?)),
         token => Err(eyre!("{:?}", token)),
     }
 }
@@ -45,6 +47,27 @@ fn eval_binop(bin_op: &BinaryOp) -> Result<Value> {
             BinaryOpKind::Div => Ok(Value::Integer(lhs / rhs)),
             op_token => todo!("op_token: {:?}", op_token),
         },
+        (Value::Integer(lhs), Value::Float(rhs)) => match bin_op.op_kind().unwrap() {
+            BinaryOpKind::Add => Ok(Value::Float(lhs as f64 + rhs)),
+            BinaryOpKind::Sub => Ok(Value::Float(lhs as f64 - rhs)),
+            BinaryOpKind::Mul => Ok(Value::Float(lhs as f64 * rhs)),
+            BinaryOpKind::Div => Ok(Value::Float(lhs as f64 / rhs)),
+            op_token => todo!("op_token: {:?}", op_token),
+        },
+        (Value::Float(lhs), Value::Integer(rhs)) => match bin_op.op_kind().unwrap() {
+            BinaryOpKind::Add => Ok(Value::Float(lhs + rhs as f64)),
+            BinaryOpKind::Sub => Ok(Value::Float(lhs - rhs as f64)),
+            BinaryOpKind::Mul => Ok(Value::Float(lhs * rhs as f64)),
+            BinaryOpKind::Div => Ok(Value::Float(lhs / rhs as f64)),
+            op_token => todo!("op_token: {:?}", op_token),
+        },
+        (Value::Float(lhs), Value::Float(rhs)) => match bin_op.op_kind().unwrap() {
+            BinaryOpKind::Add => Ok(Value::Float(lhs + rhs)),
+            BinaryOpKind::Sub => Ok(Value::Float(lhs - rhs)),
+            BinaryOpKind::Mul => Ok(Value::Float(lhs * rhs)),
+            BinaryOpKind::Div => Ok(Value::Float(lhs / rhs)),
+            op_token => todo!("op_token: {:?}", op_token),
+        },
     }
 }
 
@@ -55,8 +78,12 @@ fn eval_unary_op(unary_op: &UnaryOp) -> Result<Value> {
             .ok_or_else(|| eyre!("Missing argument for unary operator"))?,
     )?;
     match rhs {
-        Value::Integer(rhs) => match unary_op.op_kind().unwrap() {
-            UnaryOpKind::Negate => Ok(Value::Integer(-rhs)),
+        Value::Integer(val) => match unary_op.op_kind().unwrap() {
+            UnaryOpKind::Negate => Ok(Value::Integer(-val)),
+            UnaryOpKind::Not => Err(eyre!("Not is not implemented")),
+        },
+        Value::Float(val) => match unary_op.op_kind().unwrap() {
+            UnaryOpKind::Negate => Ok(Value::Float(-val)),
             UnaryOpKind::Not => Err(eyre!("Not is not implemented")),
         },
     }
@@ -76,6 +103,9 @@ mod tests {
     #[case::three_no_space("1+2+3", Value::Integer(6))]
     #[case::lhs_neg("-1 + 2", Value::Integer(1))]
     #[case::rhs_neg("1 + -2", Value::Integer(-1))]
+    #[case::lhs_float("1.0 + 2", Value::Float(3.0))]
+    #[case::rhs_float("1 + 2.0", Value::Float(3.0))]
+    #[case::both_float("1.0 + 2.0", Value::Float(3.0))]
     fn simple_additions(#[case] code: &str, #[case] expected: Value) {
         assert_eq!(super::code(code).unwrap(), expected);
     }
@@ -87,6 +117,9 @@ mod tests {
     #[case::three_no_space("1-2-3", Value::Integer(-4))]
     #[case::lhs_neg("-1 - 2", Value::Integer(-3))]
     #[case::rhs_neg("1 - -2", Value::Integer(3))]
+    #[case::lhs_float("1.0 - 2", Value::Float(-1.0))]
+    #[case::rhs_float("1 - 2.0", Value::Float(-1.0))]
+    #[case::both_float("1.0 - 2.0", Value::Float(-1.0))]
     fn simple_substractions(#[case] code: &str, #[case] expected: Value) {
         assert_eq!(super::code(code).unwrap(), expected);
     }
@@ -98,6 +131,9 @@ mod tests {
     #[case::three_no_space("1*2*3", Value::Integer(6))]
     #[case::lhs_neg("-1 * 2", Value::Integer(-2))]
     #[case::rhs_neg("1 * -2", Value::Integer(-2))]
+    #[case::lhs_float("1.0 * 2", Value::Float(2.0))]
+    #[case::rhs_float("1 * 2.0", Value::Float(2.0))]
+    #[case::both_float("1.0 * 2.0", Value::Float(2.0))]
     fn simple_multiplication(#[case] code: &str, #[case] expected: Value) {
         assert_eq!(super::code(code).unwrap(), expected);
     }
@@ -107,6 +143,9 @@ mod tests {
     #[case::three_summands("12 / 2 / 3", Value::Integer(2))]
     #[case::lhs_neg("-4 / 2", Value::Integer(-2))]
     #[case::rhs_neg("4 / -2", Value::Integer(-2))]
+    #[case::lhs_float("1.0 / 2", Value::Float(0.5))]
+    #[case::rhs_float("1 / 2.0", Value::Float(0.5))]
+    #[case::both_float("1.0 / 2.0", Value::Float(0.5))]
     // Regular nix would parse this as a path, not a division.
     // #[case::two_no_space("4/2", Value::Integer(2))]
     // #[case::three_no_space("12/2/3", Value::Integer(2))]

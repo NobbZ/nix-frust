ee813cb3b1e1137954f3db5d3e387e324c16e152
Norbert Melzer <timmelzer@gmail.com>
2023-10-14 22:44:06 +0200

    feature: list parser

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -13,13 +13,21 @@ pub enum Expr {
     Flt(f64),
     Neg(Box<Self>),
     Parens(Box<Self>),
+    List(Vec<Self>),
 }
 
 pub fn parser<'a>() -> impl Parser<'a, &'a str, Expr, extra::Err<Rich<'a, char>>> {
     recursive(|expr| {
-        choice((float::float(), integer::integer())).or(expr
-            .delimited_by(just('('), just(')'))
-            .map(|expr| Expr::Parens(Box::new(expr))))
+        choice((float::float(), integer::integer()))
+            .or(expr
+                .clone()
+                .delimited_by(just('('), just(')'))
+                .map(|expr| Expr::Parens(Box::new(expr))))
+            .or(expr
+                .repeated()
+                .collect()
+                .delimited_by(just('['), just(']'))
+                .map(Expr::List))
     })
 }
 
@@ -54,14 +62,24 @@ mod tests {
         };
     }
 
+    macro_rules! list {
+        ($($e:expr),*) => {
+            Expr::List(vec![$($e),*])
+        };
+    }
+
     #[rstest]
-    #[case::post_int("1", int!(1))]
-    #[case("1.0", flt!(1.0))]
+    #[case::pos_int("1", int!(1))]
+    #[case::pos_flt("1.0", flt!(1.0))]
     #[case::neg_int("-1", neg!(int!(1)))]
-    #[case("-1.0", flt!(-1.0))]
+    #[case::neg_flt("-1.0", flt!(-1.0))]
     #[case::par_int("(1)", parens!(int!(1)))]
     #[case::par_neg_int("(-11)", parens!(neg!(int!(11))))]
-    #[case("(1.1)", parens!(flt!(1.1)))]
+    #[case::par_flt("(1.1)", parens!(flt!(1.1)))]
+    #[case::list_empty("[]", list!())]
+    #[case::list_one("[1]", list!(int!(1)))]
+    #[case::list_two("[1 2]", list!(int!(1), int!(2)))]
+    #[case::list_het("[1 2.1]", list!(int!(1), flt!(2.1)))]
     fn numbers(#[case] code: &str, #[case] expected: Expr) {
         let parse_result = parser().parse(code);
         let expr = parse_result.output();
